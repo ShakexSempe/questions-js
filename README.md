d1bc5d50af69a915f7d98158a3acd8e161447545
Ray-Shakex <bshakex@gmail.com>
2021-04-19 19:44:33 +0200

    using selector inside element emthod

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Vanilla Javascript tutorial project #6 presented by John Smilga
 
 
 .question-text and minus-icon classes initially set to display none.
+
+
+//2. using selectors inside the element
+question arguemnt in forEach loop references individual article and the btn variable within the loop references the actual  question-buttons in the articles.
+Add event listener to each iteration
--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-//using selectors inside the element
+//2. using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+    // console.log(question);
+    const button = question.querySelector('.question-btn');
+    // console.log(button);
+    button.addEventListener('click', () => {
+        question.classList.toggle('show-text');
+    })
+});
+
+
+
+
+
 
 
 //1 traversing the dom
 
-const buttons = document.querySelectorAll('.question-btn');
+// const buttons = document.querySelectorAll('.question-btn');
 
-buttons.forEach(button => {
-    button.addEventListener('click', e => {
-        //parent of button = question-title. Parent of question-title is .question class
-        //we need to add the .show-text to. 
-        const question = e.currentTarget.parentElement.parentElement;
+// buttons.forEach(button => {
+//     button.addEventListener('click', e => {
+//         //parent of button = question-title. Parent of question-title is .question class
+//         //we need to add the .show-text to. 
+//         const question = e.currentTarget.parentElement.parentElement;
 
-        question.classList.toggle('show-text');
-    })
-})
+//         question.classList.toggle('show-text');
+//     })
+// });
